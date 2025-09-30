c35eb1449f00baec8d01789ff1b16aadeabdae7e
Muhammadtobal <tobalmuhammad600@gmail.com>
2025-09-30 22:10:02 +0300

    done

--- src/dd/dd.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DdService } from './dd.service';
-import { DdResolver } from './dd.resolver';
-
-@Module({
-  providers: [DdResolver, DdService],
-})
-export class DdModule {}
--- src/dd/dto/update-dd.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreateDdInput } from './create-dd.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdateDdInput extends PartialType(CreateDdInput) {
-  @Field(() => Int)
-  id: number;
-}
